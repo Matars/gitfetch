9aeadd887fdd4b3991357e4cf25b5631601b5adc
ilonic23 <ilia.pwl@gmail.com>
2025-10-27 06:16:10 +0200

    Delete default.nix

--- default.nix
+++
@@ -1,7 +0,0 @@
-let
-  flake-compat = import (builtins.fetchTarball {
-    url = "https://github.com/edolstra/flake-compat/archive/master.tar.gz";
-  }) { src = ./.; };
-  system = builtins.currentSystem;
-in
-  flake-compat.defaultNix.packages.${system}.default
